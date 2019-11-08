7a1c17204fc8dddd9bc447455cdea093ad765dba
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2019-11-08 09:06:47 +0530

    Updates

---
+++ precache-manifest.1beefce4278f83021d6a40f1f2255e82.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefc7210cfe2cb4c137a384451700035",
+    "url": "/vspace-react-pro/index.html"
+  },
+  {
+    "revision": "f44a2ad812eca915ce8f",
+    "url": "/vspace-react-pro/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "57503ec64f0d51af2e6f",
+    "url": "/vspace-react-pro/static/js/2.fcc9d2a5.chunk.js"
+  },
+  {
+    "revision": "f44a2ad812eca915ce8f",
+    "url": "/vspace-react-pro/static/js/main.be25a851.chunk.js"
+  },
+  {
+    "revision": "d0d3d49fd2d56e816837",
+    "url": "/vspace-react-pro/static/js/runtime-main.30882c16.js"
+  },
+  {
+    "revision": "084e5a59aec520715a1e82f1fd96fd94",
+    "url": "/vspace-react-pro/static/media/1.084e5a59.png"
+  },
+  {
+    "revision": "a0586d1a399b408d09028d3da8f46cdd",
+    "url": "/vspace-react-pro/static/media/2.a0586d1a.png"
+  },
+  {
+    "revision": "0a4721a8236efcbe3f0539c5b98b78b8",
+    "url": "/vspace-react-pro/static/media/3.0a4721a8.png"
+  },
+  {
+    "revision": "25292308e75f893425deb4d96c407c33",
+    "url": "/vspace-react-pro/static/media/4.25292308.png"
+  },
+  {
+    "revision": "be55fbfa4e13244af716f4f3efca3c16",
+    "url": "/vspace-react-pro/static/media/5.be55fbfa.png"
+  },
+  {
+    "revision": "8a6d7169e5dba976281db386a6aad2bf",
+    "url": "/vspace-react-pro/static/media/6.8a6d7169.png"
+  },
+  {
+    "revision": "9cb1f8563dedd18747ac18b600e9849b",
+    "url": "/vspace-react-pro/static/media/banner.9cb1f856.png"
+  },
+  {
+    "revision": "ccd8b11cbdae8676ce87fe488bc3ebd8",
+    "url": "/vspace-react-pro/static/media/geo-fenced.ccd8b11c.jpg"
+  },
+  {
+    "revision": "fadb0d5ba2b0acc21eb36229abf0a856",
+    "url": "/vspace-react-pro/static/media/gito.fadb0d5b.png"
+  },
+  {
+    "revision": "745e5d73f0f46b743e21de60ba7d0d1a",
+    "url": "/vspace-react-pro/static/media/multi-currency.745e5d73.jpg"
+  },
+  {
+    "revision": "f143cc9a8e14c99c72de0245148c2d0b",
+    "url": "/vspace-react-pro/static/media/p1.f143cc9a.png"
+  },
+  {
+    "revision": "3c0de3f20bed2f6cfc1d0f2c7a64f608",
+    "url": "/vspace-react-pro/static/media/p2.3c0de3f2.png"
+  },
+  {
+    "revision": "67370089748cf552431a258257bfbd11",
+    "url": "/vspace-react-pro/static/media/p3.67370089.png"
+  },
+  {
+    "revision": "ca0dc4d7d881e7fca85bf021ccfc4df9",
+    "url": "/vspace-react-pro/static/media/p4.ca0dc4d7.png"
+  },
+  {
+    "revision": "8b2edb113dfb75d1d14f1e327481e1f8",
+    "url": "/vspace-react-pro/static/media/p5.8b2edb11.png"
+  }
+]);
